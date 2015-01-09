dbc29c7bdb2f357ab62548d726edbf6a07c0f518
AngelODeath <angelodeath@outlook.com>
2015-01-09 22:58:53 +0000

    Add twitter bootsrap

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var favicon = require('serve-favicon');
+var path = require('path'); 
+var favicon = require('serve-favicon'); 
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var search = require('./routes/search');
 
 var app = express();
 
@@ -16,22 +14,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(session({
-    secret: 'ssshhhhh',
-    saveUninitialized: true,
-    resave: true
-}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.use(express.compiler({ src : __dirname + '/public', enable: ['less']}));
+
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +36,6 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
 // error handlers
 
 // development error handler
@@ -62,15 +58,23 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: {}
     });
-});
+}
+        
+// Now less files with @import 'whatever.less' will work(https://github.com/senchalabs/connect/pull/174)
+var TWITTER_BOOTSTRAP_PATH = './vendor/twitter/bootstrap/less';
+express.compiler.compilers.less.compile = function(str, fn){
+  try {
+    var less = require('less');var parser = new less.Parser({paths: [TWITTER_BOOTSTRAP_PATH]});
+    parser.parse(str, function(err, root){fn(err, root.toCSS());});
+  } catch (err) {fn(err);}
+}
 
-app.get("/*", function(req, res, next){
-    console.log('test');
-    if(typeof req.cookies['connect.sid'] !== 'undefined'){
-        console.log(req.cookies['connect.sid']);
-    }
 
-    next(); // call the next middleware
-});
+// App code
 
-module.exports = app;
+// Routes
+app.use('/', routes);
+app.use('/users', users);
+
+
+module.exports = app; 
